362400e004ef37094351b2a222bac6102c9162cf
hkunzler <hkunzler@gmail.com>
2023-09-27 23:35:18 -0600

    Create admin.svelte

---
+++ src/routes/admin.svelte
@@ -0,0 +1,9 @@
+<!-- src/routes/admin.svelte -->
+<script>
+  // You can add scripts related to your admin page here.
+</script>
+
+<main>
+  <h1>Admin Page</h1>
+  <!-- Admin content goes here -->
+</main>
