f41cf7d7455aa7d0d2bac6c0218240a5c054ad42
hkunzler <hkunzler@gmail.com>
2023-09-27 23:23:23 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
+    import { onMount } from 'svelte';
+
     import AboutUs from '../components/AboutUs.svelte';
     import ContactUs from '../components/ContactUs.svelte';
     import Footer from '../components/Footer.svelte';
     import Header from '../components/Header.svelte'
     import Landing from '../components/Landing.svelte';
     import Services from '../components/Services.svelte';
+
+    onMount(() => {
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", (user) => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+      });
 </script>
 <Header/>
 <Landing/>
