9f64cb3865a052d1b8cc382c3c1bc9734e362856
hkunzler <hkunzler@gmail.com>
2023-09-28 06:02:55 +0000

    auto open

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <script>
-    import { onMount } from 'svelte';
-
     import AboutUs from '../components/AboutUs.svelte';
     import ContactUs from '../components/ContactUs.svelte';
     import Footer from '../components/Footer.svelte';
     import Header from '../components/Header.svelte'
     import Landing from '../components/Landing.svelte';
     import Services from '../components/Services.svelte';
-
-    onMount(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.init();
-      
-      window.netlifyIdentity.on("init", (user) => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-
-          // Open the widget if the user is not logged in
-          window.netlifyIdentity.open();
-        }
-      });
-    }
-  });
 </script>
 <Header/>
 <Landing/>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,24 @@
 <!-- src/routes/admin.svelte -->
 <script>
-  // You can add scripts related to your admin page here.
-</script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.init();
+        
+        window.netlifyIdentity.on("init", (user) => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+
+            // Open the widget if the user is not logged in
+            window.netlifyIdentity.open();
+          }
+        });
+      }
+    });
+  </script>
 
 <main>
               <div data-netlify-identity-menu></div>
